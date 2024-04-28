67267e5fcbfbe46d463e31f869171106e5ac0a37
Billsoul <gtsoulbill@gmail.com>
2024-04-29 00:28:39 +0800

    recommit stocklinebot.py

--- stocklinebot.py
+++ stocklinebot.py
@@ -11,6 +11,7 @@ Original file is located at
 #!pip install line-bot-sdk
 
 #!ngrok authtoken 2fBIuRa6RZxUgFJF032HEXCYbES_4Kn5KsS5WbTUwZwzCkJvL #你自己的ngrok Token
+./ngrok authtoken 2fBIuRa6RZxUgFJF032HEXCYbES_4Kn5KsS5WbTUwZwzCkJvL
 #StockBot_01
 Line_Channel_Access_Token='YJIsZbfckdYHnzRky18KwXsUTG6BpuoW2CbTmMbN8d0mjOZVAgilQfzWjTn4jdfAc+gd1yYy1LKbfubnEYDB0phYOZdq93gjAfPanniTJSC29B/R9Gu9XuadTO+aT3KmZ/onslv+dirMClU72klXBgdB04t89/1O/w1cDnyilFU='
 Line_Channel_Secret='d305cbeb1f356533d1db5ce07081cf27'
